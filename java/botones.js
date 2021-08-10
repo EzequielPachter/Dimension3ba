290ccfe05c5da9bb304bfc4e9149b17ffea13de2
Ezequiel <pachterezequiel@gmail.com>
2021-08-10 19:09:12 -0300

    Base de carga de  archivos
    Esto es una base de lo que sera el E-comerce, aun no hay funcionalidades, solo la parte de agregar cantidades y guardar ese dato

---
+++ java/botones.js
@@ -0,0 +1,23 @@
+      let botonInfoEnvios = document.getElementById("info-envios")
+
+      botonInfoEnvios.addEventListener("click", respuestaClick)
+      function respuestaClick(){
+        alert("Respuesta evento");
+      }
+      const precioTuqueras = 1000; 
+      let tuq = document.getElementById("add-cart-tuqueras")
+
+      tuq.addEventListener("click", agregarTuqueras)
+      function agregarTuqueras(){
+        let cantidadTuq = parseInt(prompt("ingrese cantidad de tuqueras que quiere comprar, el valor es por 10U"));
+        console.log(cantidadTuq);
+        var totalTuqueras = precioTuqueras * cantidadTuq;
+        console.log(totalTuqueras);
+      }
+      
+      let Carro = document.getElementById("cart")
+      Carro.addEventListener("click", totalCompra)
+      function totalCompra(){
+        totalcompra = totalTuqueras + " "; 
+        alert(totalcompra);
+      }
